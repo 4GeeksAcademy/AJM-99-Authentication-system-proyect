97db654e74c79cb16eddf79286a705e9e2b02ab7
AJM-99 <a.j.morenoalb@gmail.com>
2026-02-11 07:20:03 +0000

    Project in progress

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from sqlalchemy import select
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +24,54 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+    
+    if not email or not password:
+        return jsonify({"error":"Missing data, please add fill all the fields"}), 400
+    
+    existing_user = db.session.execute(select(User).where(User.email == email)).scalar_one_or_none()
+    
+    if existing_user:
+        return jsonify({"error":"There is already an account associated to this email"}), 400
+    
+    new_user = User (email = email)
+    new_user.set_password(password)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify("Your user has been created successfully"), 201
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"error":"Missing data, please fill all the fields"}), 400
+    
+    existing_user = db.session.execute(select(User).where(User.email == email)).scalar_one_or_none()
+
+    if not existing_user or not existing_user.check_password(password):
+        return jsonify({"error":"Incorrect email or password"}), 400
+    
+    access_token = create_access_token(identity=str(existing_user.id))
+    return jsonify({"msg":"Login successfull", "token": access_token}), 200
+
+@api.route('/theXfiles', methods=['GET'])
+@jwt_required()
+def access_to_the_X_files():
+    user_id = get_jwt_identity()
+    user = db.session.get(User, int(user_id))
+    if not user:
+        return jsonify({"error":"User not found"}), 404
+    return jsonify("W3lc0m3, y0u hav3 acc3ss t0 th3 X fil3s, n0w y0u kn0w th3 truth. B3 CAR3FUL!!"), 200
+
+
+    
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,10 +1,31 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
+	const [ user, setUser ] = useState({
+		email: "",
+		password: ""
+	})
+
+	const handleChange = (e) => {
+		setUser({
+			...user,
+			[e.target.name]:e.target.value
+		})
+	}
+
+		const handleSubmit = (e) => {
+			e.preventDefault()
+			if (!user.email || !user.password){
+				alert("All fields are required")
+			}
+		}
+
+
+	console.log(user)
 
 	const loadMessage = async () => {
 		try {
@@ -33,20 +54,19 @@ export const Home = () => {
 	}, [])
 
 	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
-			</div>
+		<div className="container mt-5">
+			<form>
+				<div className="mb-3">
+
+					<label htmlFor="email" className="form-label">Email</label>
+					<input type="text" name="email" placeholder="Enter your email" className="form-control" value={user.email} onChange={handleChange}></input>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="password" className="form-label">Password</label>
+					<input type="text" name="password" placeholder="Enter your password" className="form-control" value={user.password} onChange={handleChange}></input>
+				</div>
+				<button type="submit" className="btn btn-warning w-100" onSubmit={handleSubmit}>Login</button>
+			</form>
 		</div>
 	);
 }; 
